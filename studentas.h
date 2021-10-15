dfeca586df27a866e49b1513aa4509a4f74debd5
gabrikole <91156170+gabrikole@users.noreply.github.com>
2021-10-15 20:39:28 +0300

    Create studentas.h

---
+++ studentas.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+
+using std::string;
+using std::vector;
+
+
+struct studentas{
+  string vardas, pavarde;
+  vector <float> paz;
+  float egz;
+  float galut;
+  vector <float> mediana;
+};
