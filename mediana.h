56c0e16894c172e3c043887a5877536bee87654f
gabrikole <91156170+gabrikole@users.noreply.github.com>
2021-10-15 20:37:04 +0300

    Create mediana.h

---
+++ mediana.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "studentas.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <stdlib.h>
+#include <sstream>
+#include <fstream>
+#include <iterator>
+
+
+float mediana(std::vector<int> pazymiai);
