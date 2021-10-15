2b67647ee9162b709b66aabceda2030898ae04fa
gabrikole <91156170+gabrikole@users.noreply.github.com>
2021-10-15 20:36:15 +0300

    Create count.h

---
+++ count.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <stdlib.h>
+#include <sstream>
+#include <fstream>
+#include <iterator>
+#include "studentas.h"
+#include "print.h"
+#include "mediana.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::setw;
+using std::setprecision;
+using std::vector;
+using std::left;
+using std::ofstream;
+using std::domain_error;
+using std::ifstream;
+
+
+unsigned int count(string const& str);
