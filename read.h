8e53cabd2629a36c257e1bd11b0211bc37e0b066
gabrikole <91156170+gabrikole@users.noreply.github.com>
2021-10-15 20:38:47 +0300

    Create read.h

---
+++ read.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "studentas.h"
+#include "count.h"
+#include "print.h"
+#include "mediana.h"
+
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <stdlib.h>
+#include <sstream>
+#include <fstream>
+#include <iterator>
+
+
+using std::vector;
+
+
+void read(vector<studentas>& Eil, int* pazymiai)
