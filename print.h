5b3f839d89cfea3b2cfb6c10cc061829806cd8ab
gabrikole <91156170+gabrikole@users.noreply.github.com>
2021-10-15 20:38:04 +0300

    Create print.h

---
+++ print.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <stdlib.h>
+#include <sstream>
+#include <fstream>
+#include <iterator>
+
+#include "studentas.h"
+#include "mediana.h"
+
+
+
+using std::vector;
+
+
+void print(vector<studentas> Eil, int pazymiu_sk);
